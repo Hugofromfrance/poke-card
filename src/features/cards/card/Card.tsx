ba32cf93e61e5aa3c3834bdd2b069ea34b2571a5
Hugofromfrance <h.hardier@gmail.com>
2022-09-15 16:07:08 +0200

    ✨  Create card component

---
+++ src/features/cards/card/Card.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { ICard } from '../../../App';
+import styles from './Card.module.css';
+import {
+    toggleOwnedCard,
+    toggleOwnedCardFromStore,
+} from './../../../features/cards/cardsSlice';
+import { useAppDispatch } from './../../../app/hooks';
+export function Card(props: any) {
+    const card = props.card;
+    const dispatch = useAppDispatch();
+    function handleCardClick(cardId: string) {
+        dispatch(toggleOwnedCard(cardId));
+        dispatch(toggleOwnedCardFromStore(cardId));
+    }
+
+    return (
+        <div className="col">
+            <div
+                className={`${styles.cursorPointer} card shadow-sm ${
+                    !card.isOwned ? styles.notOwned : ''
+                }`}
+                onClick={() => handleCardClick(card._id)}
+            >
+                <img
+                    className="mx-auto"
+                    src={card.image}
+                    alt={card.name}
+                    width="150"
+                />
+                <div className="card-body">
+                    <p className="card-text font-weight-bold">{card.name}</p>
+                    <div>
+                        <p>HP: {card.stats.HP}</p>
+                    </div>
+                    <div className="d-flex flex-row">
+                        {card.apiTypes.map((type: ICard, index: number) => (
+                            <img
+                                key={`${card.image}${index}`}
+                                src={type.image}
+                                alt={type.name}
+                                height="30"
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
