e144525f77bd3d5c35bf7ad6637a81bf987a64e7
Hugofromfrance <h.hardier@gmail.com>
2023-01-22 12:43:21 +0100

    🔥 Remove old folders

--- src/app/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import cardsReducer from '../features/cards/cardsSlice';
-
-const rootReducer = combineReducers({
-    cards: cardsReducer,
-});
-
-export default rootReducer;
